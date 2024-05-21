5696cd9a41626cd436aa8d8401497f4be39b8b25
iIvan13 <todelanoivan13@gmail.com>
2024-05-21 18:18:43 -0500

    pwa vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,23 @@
 import { defineConfig } from 'astro/config';
 import netlify from "@astrojs/netlify";
+import { VitePWA } from 'vite-plugin-pwa';
+import { manifest } from '/src/scripts/configPWA';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: netlify()
+  adapter: netlify(),
+  vite:{
+    plugins:[
+      VitePWA({
+        registerType: 'autoUpdate',
+        manifest,
+        workbox:{
+          globDirectory: 'dist',
+          globPatterns: ['**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}'],
+          navigateFallback: null,
+        }
+      })
+    ]
+  }
 });
---
+++ src/scripts/configPWA.ts
@@ -0,0 +1,63 @@
+import type { ManifestOptions } from "vite-plugin-pwa";
+
+export const manifest: Partial<ManifestOptions> = {
+    name: 'Ivan - Portafolio',
+    short_name: 'Ivan',
+    description:
+        "Este es mi portafolio, creado con Astro PWA Starter, una plantilla de Astro para construir sitios web estáticos robustos.",
+    background_color: '#2B2B2B',
+    theme_color: '#090a20',
+    display: 'minimal-ui',
+    icons: [
+        {
+            src: './src/assets/img/icon_1024.png',
+            sizes: '1024x1018',
+            type: 'image/png'
+        },
+        {
+            src: './src/assets/img/icon_512.png',
+            sizes: '512x509',
+            type: 'image/png'
+        },
+        {
+            src: './src/assets/img/icon_384.png',
+            sizes: '384x382',
+            type: 'image/png'
+        },
+        {
+            src: './src/assets/img/icon_256.png',
+            sizes: '256x255',
+            type: 'image/png'
+        },
+        {
+            src: './src/assets/img/icon_192.png',
+            sizes: '192x191',
+            type: 'image/png'
+        },
+        {
+            src: './src/assets/img/icon_128.png',
+            sizes: '128x128',
+            type: 'image/png'
+        },
+        {
+            src: './src/assets/img/icon_96.png',
+            sizes: '96x96',
+            type: 'image/png'
+        },
+        {
+            src: './src/assets/img/icon_64.png',
+            sizes: '64x64',
+            type: 'image/png'
+        },
+        {
+            src: './src/assets/img/icon_32.png',
+            sizes: '32x32',
+            type: 'image/png'
+        },
+        {
+            src: './src/assets/img/icon_16.png',
+            sizes: '16x15',
+            type: 'image/png'
+        }
+    ] 
+}
